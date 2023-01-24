b262cc56553376805222615cc3db6c198a0c6a8c
anthonymarie22 <72616953+anthonymarie22@users.noreply.github.com>
2023-01-23 23:53:48 -0500

    Initial Commit

---
+++ Github Exeercise/Github_ExeerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExeerciseApp.swift
+//  Github Exeercise
+//
+//  Created by Anthony Marie on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExeerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
